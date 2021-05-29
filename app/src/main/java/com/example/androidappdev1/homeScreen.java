440158ba905a88b296f42efb6777ccfbbd388093
shauryamat20 <68394555+shauryamat20@users.noreply.github.com>
2021-05-29 15:04:33 -0500

    Editing Budget UI

--- app/src/main/java/com/example/androidappdev1/homeScreen.java
+++ app/src/main/java/com/example/androidappdev1/homeScreen.java
@@ -2,7 +2,9 @@ package com.example.androidappdev1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class homeScreen extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class homeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+    }
+    public void changeBudget(View view) {
+        Intent intent = new Intent(this, editBudget.class);
+        startActivity(intent);
     }
 }
